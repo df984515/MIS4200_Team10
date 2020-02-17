8b0ab649e77b72f2f4e2c8ca1ccd602f4ed21148
Foster <df984515@ohio.edu>
2020-02-17 11:25:59 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MIS4200_Team10.Startup))]
+namespace MIS4200_Team10
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
